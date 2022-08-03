5cb056d9ac9f8c865d40ee5466984ca8365652a0
Annu <annusinghraj1998@gmail.com>
2022-08-03 22:05:02 +0530

    ADDED: Added user name regex has minimum three character

--- src/main/java/com/bridgelabz/LoginServlet.java
+++ src/main/java/com/bridgelabz/LoginServlet.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.regex.Pattern;
 
 
 @WebServlet(
@@ -26,7 +27,8 @@ public class LoginServlet extends HttpServlet {
         String pwd = request.getParameter("pwd");
         String userId = getServletConfig().getInitParameter("user");
         String password = getServletConfig().getInitParameter("password");
-        if (userId.equals(user) && password.equals(pwd)) {
+        if (Pattern.matches("[A-Z][a-z]{2,}", userId) &&
+                userId.equals(user) && password.equals(pwd)) {
             request.setAttribute("user", user);
             request.getRequestDispatcher("LoginSuccess.jsp").forward(request, response);
         } else {
